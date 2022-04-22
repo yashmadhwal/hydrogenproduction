863d0d4aba6001a47672cf2e1ce65fdc0775eba7
yashmadhwal <yashmadhwal@yahoo.co.in>
2022-04-22 11:23:10 +0300

    ERC1155 Basic workflow indicating production and transfering of Energies as per workflow

--- contracts/HydrogenProductions.sol
+++ contracts/HydrogenProductions.sol
@@ -1,18 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
+// ________________ ERC 1155 _________________________
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "hardhat/console.sol";
 
-// Importing Libraries
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import '@openzeppelin/contracts/access/Ownable.sol';
-import 'hardhat/console.sol';
+contract SUPPLYHYDROGEN is ERC1155 {
+    // assigning Batch Ids
+    uint256 public constant WATER = 0;
+    uint256 public constant RENEWABLES = 1;
+    uint256 public constant HYDROGEN_FUEL = 2;
 
-contract HydrogenProduction is ERC20, Ownable, ERC20Burnable {
+    // Self Instance
+    IERC1155 public h2Token;
 
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+    // assigning Addresses of the producer
+    address public WATER_PROVIDER;
+    address public RENEWABLES_PROVIDER;
+    address public FUEL_GENERATOR;
 
-    function mintingEnergy(uint _amount) public onlyOwner{
-        require(_amount !=0,'Cannot produce zero energy');
-        _mint(msg.sender, _amount);
+    constructor(address _water, address _energy, address _fuel) ERC1155("") {
+        h2Token = IERC1155(address(this));
+
+        WATER_PROVIDER = _water;
+        RENEWABLES_PROVIDER = _energy;
+        FUEL_GENERATOR = _fuel;
+    }
+
+    function mintEnergy(uint _amountEnergy) public onlyRenewable{
+        require(_amountEnergy != 0,'Invalid amount of Energy');
+        _mint(msg.sender, RENEWABLES, _amountEnergy, "");
+    }
+
+    function mintWater(uint _amountWater) public onlyWater{
+        require(_amountWater != 0,'Invalid amount of Energy');
+        _mint(msg.sender, WATER, _amountWater, "");
+    }
+
+    function mintHydrogen() public {
+
+        uint _balanceEnergy = h2Token.balanceOf(msg.sender,RENEWABLES);
+        uint _balanceWater = h2Token.balanceOf(msg.sender,WATER);
+        // todo: Correct H2 Formula to check (require statement)
+        uint h2 = _balanceEnergy + _balanceWater;
+        _mint(msg.sender, HYDROGEN_FUEL, h2 , "");
+        _burn(msg.sender, 0, _balanceWater);
+        _burn(msg.sender, 1, _balanceEnergy);
+
+    }
+
+    modifier onlyRenewable(){
+        require(msg.sender == RENEWABLES_PROVIDER, 'Not Authorised Access');
+        _;
+    }
+
+    modifier onlyWater(){
+        require(msg.sender == WATER_PROVIDER, 'Not Authorised Access');
+        _;
     }
 }
+
--- test/test_HydrogenProduction.js
+++ test/test_HydrogenProduction.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("H2 Contract Testing", function () {
+describe("Phase 1", function () {
 
     let contractOwnerProducer, contractAddress;
     let h2p; // Deployed instance
@@ -11,131 +11,145 @@ describe("H2 Contract Testing", function () {
     let burnElectrolysis;
     before( async () =>{
         //  Getting list of signers
-        [contractOwnerProducer,electrolysis,distribution1,distribution2,dispensing1,dispensing2,motor1] =  await ethers.getSigners();
-        const H2P = await ethers.getContractFactory("HydrogenProduction");
-        h2p  = await H2P.deploy('HydrogenProduction','H2P');
+
+        [contractOwnerProducer,renewables,waterSupplier,electrolysis] =  await ethers.getSigners();
+        const H2P = await ethers.getContractFactory("SUPPLYHYDROGEN");
+        h2p  = await H2P.deploy(waterSupplier.address,renewables.address,electrolysis.address);
 
         // Storing contract address
         contractAddress = h2p.address
-
-        // assuming batch of energy in units = 5000 units
-        batchOfEnergy = 5000;
-
     });
 
     describe('Basic Contract Deployment', () => {
-        it('Contract Deployed with correct name', async function () {
-            expect(await h2p.name()).to.equal('HydrogenProduction')
-        });
-
-        it('Contract Deployed with correct symbol', async function () {
-            expect(await h2p.symbol()).to.equal('H2P')
-        });
-
-        it('Contract Deployed with zero circulation', async function () {
-            expect(await h2p.totalSupply()).to.equal(0)
-        });
-
-        it('Contract owner is correct', async function () {
-            expect(await h2p.owner()).to.equal(contractOwnerProducer.address);
-        });
 
         it('Contract deployed address is not null', async function () {
             expect(h2p.address).not.equal(0);
         });
 
+        it('WATER generator address is correctly deployed', async function () {
+            expect(await h2p.WATER_PROVIDER()).equal(waterSupplier.address);
+        });
+
+        it('Renewable generator address is correctly deployed', async function () {
+            expect(await h2p.RENEWABLES_PROVIDER()).equal(renewables.address);
+        });
+
+        it('WATER generator address is correctly deployed', async function () {
+            expect(await h2p.FUEL_GENERATOR()).equal(electrolysis.address);
+        });
     });
 
-    describe('Generating Energy', () => {
-        it('Renewables can create zero tokens', async function () {
-            await expect(h2p.mintingEnergy(0)).to.revertedWith('Cannot produce zero energy')
+    describe('Production Phase', () => {
+        it('Renewables cannot generate Zero Energy', async function () {
+            await expect(h2p.connect(renewables).mintEnergy(0)).to.revertedWith('Invalid amount of Energy')
+        });
+
+        it('Non authorised renewable energy makers cannot make energy', async function () {
+            await expect(h2p.mintEnergy(1000)).to.revertedWith('Not Authorised Access')
+        });
+
+        it('Renewables cannot generate Zero Energy', async function () {
+            await expect(h2p.connect(waterSupplier).mintWater(0)).to.revertedWith('Invalid amount of Energy')
+        });
+
+        it('Non authorised renewable energy makers cannot make energy', async function () {
+            await expect(h2p.mintWater(1000)).to.revertedWith('Not Authorised Access')
         });
 
         it('Renewables can create tokens', async function () {
-            await expect(h2p.mintingEnergy(batchOfEnergy)).not.reverted
+            // Minting 1000 batches each
+            await h2p.connect(waterSupplier).mintWater(1000);
+            await h2p.connect(renewables).mintEnergy(1000);
+
+            // Transferring
+            await h2p.connect(waterSupplier).safeTransferFrom(waterSupplier.address,electrolysis.address, 0, 1000, '0x0000000000000000000000000000000000000000000000000000000000000000')
+            await h2p.connect(renewables).safeTransferFrom(renewables.address,electrolysis.address, 1, 1000, '0x0000000000000000000000000000000000000000000000000000000000000000')
+
+            // minting and buring.
+            await h2p.connect(electrolysis).mintHydrogen();
         });
 
-        it('Batch of energy in circulation: 5000 ', async function () {
-            expect (await h2p.totalSupply()).to.equal(batchOfEnergy);
-        });
-
-        it('Only contract owner can mint energy', async function () {
-            await expect(h2p.connect(electrolysis).mintingEnergy(batchOfEnergy)).to.revertedWith('Ownable: caller is not the owner');
-        });
+        // it('Batch of energy in circulation: 5000 ', async function () {
+        //     expect (await h2p.totalSupply()).to.equal(batchOfEnergy);
+        // });
+        //
+        // it('Only contract owner can mint energy', async function () {
+        //     await expect(h2p.connect(electrolysis).mintingEnergy(batchOfEnergy)).to.revertedWith('Ownable: caller is not the owner');
+        // });
     });
-
-    describe('Transferring Energy', () => {
-        it('Transfer energy to electrolysis ', async function () {
-            // const tranferUnit = batchOfEnergy/2;
-            await expect(h2p.transfer(electrolysis.address,batchOfEnergy)).not.reverted
-        });
-
-        it('electrolysis receive less amount of energy to it burns the not received amount', async function () {
-            burnElectrolysis = batchOfEnergy * (0.05)
-
-            await expect(h2p.connect(electrolysis).burn(burnElectrolysis)).not.reverted
-        });
-
-        it('Renewable energy should have zero energy stored', async function () {
-            expect(await h2p.balanceOf(contractOwnerProducer.address)).to.equal(0);
-        });
-
-        it('Cannot transfer more energy because balance is zero', async function () {
-            // const tranferUnit = batchOfEnergy/2;
-            await expect(h2p.transfer(electrolysis.address,batchOfEnergy)).to.revertedWith('ERC20: transfer amount exceeds balance')
-        });
-
-        it('Electrolysis transfers to distributor1 full batch', async function () {
-            // Getting amount of Energy Electrolysis has
-            const energyElectrolysis = await h2p.balanceOf(electrolysis.address);
-            // console.log('energyElectrolysis',energyElectrolysis)
-            await expect(h2p.connect(electrolysis).transfer(distribution1.address,energyElectrolysis)).not.reverted
-        });
-
-        it('Renewable Generates one more batch while first batch is with distribution1', async function(){
-            await expect(h2p.mintingEnergy(batchOfEnergy)).not.reverted
-        });
-        //
-        it('Batch of energy in circulation: 10000 minus the burned amount by electrolysis', async function () {
-            // ToDo: To fined the burned amount
-            expect (await h2p.totalSupply()).to.equal((batchOfEnergy * 2)-burnElectrolysis);
-        });
-
-        it('New batch is distributed to distributor 2 via electrolysis after burning', async function () {
-            await expect(h2p.transfer(electrolysis.address,batchOfEnergy)).not.reverted
-
-            // That mean it has burned 2 times
-            await expect(h2p.connect(electrolysis).burn(burnElectrolysis)).not.reverted
-
-            //Getting balance of electroolysis
-            const energyElectrolysis = await h2p.balanceOf(electrolysis.address);
-
-            await expect(h2p.connect(electrolysis).transfer(distribution2.address,energyElectrolysis)).not.reverted
-        });
-
-        it(`Fast-forward: Both distributors forward it to despensing stations (1 and 2)`,async function () {
-            await expect(h2p.connect(distribution1).transfer(dispensing1.address,batchOfEnergy - burnElectrolysis)).not.reverted
-            await expect(h2p.connect(distribution2).transfer(dispensing2.address,batchOfEnergy - burnElectrolysis)).not.reverted
-        });
-
-        it('Batch of energy in circulation: 10000 minus the burned amount by electrolysis x 2', async function () {
-            // ToDo: To fined the burned amount
-            expect (await h2p.totalSupply()).to.equal((batchOfEnergy * 2) - (2 * burnElectrolysis));
-        });
-
-
-        //
-        it(`Burning one batch of tokens`,async function () {
-            await expect(h2p.connect(dispensing1).burn(batchOfEnergy - burnElectrolysis)).not.reverted
-        });
-        //
-        it(`Total supply should reduce by one batch`,async function () {
-            expect(await h2p.totalSupply()).to.equal(batchOfEnergy - burnElectrolysis)
-        });
-        //
-        it(`Burning last batch of tokens, and total supply in circulation should be zero `,async function () {
-            await expect(h2p.connect(dispensing2).burn(batchOfEnergy - burnElectrolysis)).not.reverted
-            expect(await h2p.totalSupply()).to.equal(0)
-        });
-    });
+    //
+    // describe('Transferring Energy', () => {
+    //     it('Transfer energy to electrolysis ', async function () {
+    //         // const tranferUnit = batchOfEnergy/2;
+    //         await expect(h2p.transfer(electrolysis.address,batchOfEnergy)).not.reverted
+    //     });
+    //
+    //     it('electrolysis receive less amount of energy to it burns the not received amount', async function () {
+    //         burnElectrolysis = batchOfEnergy * (0.05)
+    //
+    //         await expect(h2p.connect(electrolysis).burn(burnElectrolysis)).not.reverted
+    //     });
+    //
+    //     it('Renewable energy should have zero energy stored', async function () {
+    //         expect(await h2p.balanceOf(contractOwnerProducer.address)).to.equal(0);
+    //     });
+    //
+    //     it('Cannot transfer more energy because balance is zero', async function () {
+    //         // const tranferUnit = batchOfEnergy/2;
+    //         await expect(h2p.transfer(electrolysis.address,batchOfEnergy)).to.revertedWith('ERC20: transfer amount exceeds balance')
+    //     });
+    //
+    //     it('Electrolysis transfers to distributor1 full batch', async function () {
+    //         // Getting amount of Energy Electrolysis has
+    //         const energyElectrolysis = await h2p.balanceOf(electrolysis.address);
+    //         // console.log('energyElectrolysis',energyElectrolysis)
+    //         await expect(h2p.connect(electrolysis).transfer(distribution1.address,energyElectrolysis)).not.reverted
+    //     });
+    //
+    //     it('Renewable Generates one more batch while first batch is with distribution1', async function(){
+    //         await expect(h2p.mintingEnergy(batchOfEnergy)).not.reverted
+    //     });
+    //     //
+    //     it('Batch of energy in circulation: 10000 minus the burned amount by electrolysis', async function () {
+    //         // ToDo: To fined the burned amount
+    //         expect (await h2p.totalSupply()).to.equal((batchOfEnergy * 2)-burnElectrolysis);
+    //     });
+    //
+    //     it('New batch is distributed to distributor 2 via electrolysis after burning', async function () {
+    //         await expect(h2p.transfer(electrolysis.address,batchOfEnergy)).not.reverted
+    //
+    //         // That mean it has burned 2 times
+    //         await expect(h2p.connect(electrolysis).burn(burnElectrolysis)).not.reverted
+    //
+    //         //Getting balance of electroolysis
+    //         const energyElectrolysis = await h2p.balanceOf(electrolysis.address);
+    //
+    //         await expect(h2p.connect(electrolysis).transfer(distribution2.address,energyElectrolysis)).not.reverted
+    //     });
+    //
+    //     it(`Fast-forward: Both distributors forward it to despensing stations (1 and 2)`,async function () {
+    //         await expect(h2p.connect(distribution1).transfer(dispensing1.address,batchOfEnergy - burnElectrolysis)).not.reverted
+    //         await expect(h2p.connect(distribution2).transfer(dispensing2.address,batchOfEnergy - burnElectrolysis)).not.reverted
+    //     });
+    //
+    //     it('Batch of energy in circulation: 10000 minus the burned amount by electrolysis x 2', async function () {
+    //         // ToDo: To fined the burned amount
+    //         expect (await h2p.totalSupply()).to.equal((batchOfEnergy * 2) - (2 * burnElectrolysis));
+    //     });
+    //
+    //
+    //     //
+    //     it(`Burning one batch of tokens`,async function () {
+    //         await expect(h2p.connect(dispensing1).burn(batchOfEnergy - burnElectrolysis)).not.reverted
+    //     });
+    //     //
+    //     it(`Total supply should reduce by one batch`,async function () {
+    //         expect(await h2p.totalSupply()).to.equal(batchOfEnergy - burnElectrolysis)
+    //     });
+    //     //
+    //     it(`Burning last batch of tokens, and total supply in circulation should be zero `,async function () {
+    //         await expect(h2p.connect(dispensing2).burn(batchOfEnergy - burnElectrolysis)).not.reverted
+    //         expect(await h2p.totalSupply()).to.equal(0)
+    //     });
+    // });
 });
